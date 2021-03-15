dd604c49c55b040c32bb2b3a4d69e3a511412a6e
Sait KARABULAK <skarabulak@eraysoft.com.tr>
2021-03-15 16:13:46 +0300

    Code Rewiev

--- MarsRover.Bussines/Services/NavigateServices.cs
+++ MarsRover.Bussines/Services/NavigateServices.cs
@@ -11,21 +11,6 @@ namespace MarsRover.Bussines.Services
     public static class NavigateServices
     {
         public static Direction direction;
-        public static void SetRoverPosition(this ProcessedCommands processedCommands)
-        {
-            if (processedCommands.StartPosition.XPoint<0 && processedCommands.StartPosition.YPoint<0)
-            {
-                processedCommands.StartPosition.XPoint = 0;
-                processedCommands.StartPosition.YPoint = 0;
-                processedCommands.CurrentPosition.XPoint = 0;
-                processedCommands.CurrentPosition.YPoint = 0;
-            }
-            else
-            {
-                processedCommands.CurrentPosition.XPoint = processedCommands.StartPosition.XPoint;
-                processedCommands.CurrentPosition.YPoint = processedCommands.StartPosition.YPoint;
-            }
-        }
         public static string CurrentPosition(this ProcessedCommands processedCommands)
         {
             return processedCommands.CurrentPosition.XPoint.ToString() + " " + processedCommands.CurrentPosition.YPoint.ToString() + " " + processedCommands.CurrentPosition.LookingDirection;
